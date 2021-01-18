e898ae08096cabe4e98ac11dbd1ae12b803a817b
Brano Zavracky <branislav.zavracky@brackets.sk>
2021-01-18 16:23:18 +0100

    add rn-handy-utils package

--- index.js
+++ index.js
@@ -6,16 +6,11 @@ import {AppRegistry} from 'react-native';
 import App from './app/App';
 import {name as appName} from './app.json';
 import { configure } from 'mobx'
+import { disableConsoleLogsInProd } from 'rn-handy-utils';
 
 configure({ enforceActions: 'never' });
+
 // !including console logs can slow down your React Native app.
-if (!__DEV__) {
-	console.log = () => {}
-	console.warn = () => {}
-	console.debug = () => {}
-	console.table = () => {}
-	console.info = () => {}
-	console.error = () => {}
-}
+disableConsoleLogsInProd()
 
 AppRegistry.registerComponent(appName, () => App);
