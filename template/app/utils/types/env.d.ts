99f5d60e373baa661d256b246a85fc2d8092fc01
Brano Zavracky <branislav.zavracky@brackets.sk>
2021-03-20 16:36:45 +0100

    style: lint

--- template/app/utils/types/env.d.ts
+++ template/app/utils/types/env.d.ts
@@ -1,7 +1,7 @@
-declare module "react-native-config" {
+declare module 'react-native-config' {
   interface Env {
-    ENV: "DEVELOPMENT" | "STAGING" | "PRODUCTION"
-    EXAMPLE_API_KEY: "csdkvnsdkjn43j5n3k4nsd"
+    ENV: 'DEVELOPMENT' | 'STAGING' | 'PRODUCTION'
+    EXAMPLE_API_KEY: 'csdkvnsdkjn43j5n3k4nsd'
   }
   const Config: Env
   export default Config
