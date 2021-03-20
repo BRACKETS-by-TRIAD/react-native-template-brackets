ee008002648a600eda20300e8f3eaf69bd60c8de
Brano Zavracky <branislav.zavracky@brackets.sk>
2021-03-20 16:21:39 +0100

    build: eslint, prettier, typescript - setup

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  "plugins": [
-    "macros",
-    'react-native-reanimated/plugin'
-  ]
-};
+  plugins: ['macros', 'react-native-reanimated/plugin'],
+}
