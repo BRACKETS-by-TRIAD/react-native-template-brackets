50817c9b28916cdcb3b8c1498a437b6cbaebb309
Brano Zavracky <branislav.zavracky@brackets.sk>
2021-09-13 12:03:57 +0200

    fix: remove old/unfinished ui-kit implementation

--- template/app/screens/HomeScree.tsx
+++ template/app/screens/HomeScree.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import { View } from 'react-native'
-import { ButtonPrimary } from '../ui-kit'
 
 export const HomeScreen = () => {
-  return (
-    <View>
-      <ButtonPrimary title="Example button" onPress={() => {}} />
-    </View>
-  )
+  return <View />
 }
--- template/app/ui-kit/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './Button'
-export * from './layout'
-export * from './Loading'
-export * from './Typography'
