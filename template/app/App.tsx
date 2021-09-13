fd634434084eb13a17aac4624e5956f806c2052e
Brano Zavracky <branislav.zavracky@brackets.sk>
2021-09-13 12:19:40 +0200

    feat: add urql

--- template/app/App.tsx
+++ template/app/App.tsx
@@ -2,6 +2,8 @@ import { i18n } from '@lingui/core'
 import { I18nProvider } from '@lingui/react'
 import React from 'react'
 import { StatusBar } from 'react-native'
+import { Provider } from 'urql'
+import { client } from './config/api'
 import { Navigation } from './config/Navigation'
 
 i18n.load('en', require('./locales/en/messages'))
@@ -9,14 +11,16 @@ i18n.activate('en')
 
 const App = () => {
   return (
-    <I18nProvider i18n={i18n}>
-      <StatusBar
-        barStyle="dark-content"
-        translucent
-        backgroundColor="transparent"
-      />
-      <Navigation />
-    </I18nProvider>
+    <Provider value={client}>
+      <I18nProvider i18n={i18n}>
+        <StatusBar
+          barStyle="dark-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <Navigation />
+      </I18nProvider>
+    </Provider>
   )
 }
 
